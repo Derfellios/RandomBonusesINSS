0bfb3e5e275aa659d6821714f0b5238b7ba84290
Derfellios <joris.nieuwveld@gmail.com>
2020-05-10 12:10:40 +0200

    Add files via upload

--- Server_Created.lua
+++ Server_Created.lua
@@ -15,27 +15,27 @@ function Server_Created(game, settings)
 			end	
 		end
 		if (bonus.Amount > 0 or Mod.Settings.AllowNegative) and Allowed then 
-		    local rndAmount = math.random(-Mod.Settings.RandomizeAmountMin, Mod.Settings.RandomizeAmountMax);
 		    if Mod.Settings.TimiRandom then
 			    local BonusSize = 0;
 				for _,_ in pairs( bonus.Territories ) do
 					BonusSize = BonusSize + 1;
 				end
-				rndAmount = math.random(BonusSize - bonus.Amount-2, BonusSize- bonus.Amount-1);
+				newBonusValueRandomized = math.random(BonusSize - 2, BonusSize - 1);
+			else 
+			    newBonusValueRandomized = bonus.Amount + math.random(-Mod.Settings.RandomizeAmountMin, Mod.Settings.RandomizeAmountMax);
 			end
-			if (rndAmount ~= 0) then --don't do anything if we're not changing the bonus.  We could leave this check off and it would work, but it show up in Settings as an overridden bonus when it's not.
+			if (newBonusValueRandomized ~= bonus.Amount) then --don't do anything if we're not changing the bonus.  We could leave this check off and it would work, but it would show up in Settings as an overridden bonus when it's not.
 
-				local newValue = bonus.Amount + rndAmount;
 				-- don't take a positive or zero bonus negative unless AllowNegative was checked.
-				if (newValue < 0 and not Mod.Settings.AllowNegative) then
-					newValue = 0;
+				if (newBonusValueRandomized < 0 and not Mod.Settings.AllowNegative) then
+					newBonusValueRandomized = 0;
 				end
 
 				-- -1000 to +1000 is the maximum allowed range for overridden bonuses, never go beyond that
-				if (newValue < -1000) then newValue = -1000 end;
-				if (newValue > 1000) then newValue = 1000 end;
+				if (newBonusValueRandomized < -1000) then newBonusValueRandomized = -1000 end;
+				if (newBonusValueRandomized > 1000) then newBonusValueRandomized = 1000 end;
 		
-				overriddenBonuses[bonus.ID] = newValue;
+				overriddenBonuses[bonus.ID] = newBonusValueRandomized;
 			end
 		end
     end
